0b9d9418fb8f4fe1375c38548fb85e56d0932cc0
Daniel Stein <stein-daniel@codingcircus.com>
2017-04-07 14:20:59 +0200

    [TASK] Init project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Editor Test</h2>
+        </div>
+        <div className="App-content">
+          <Editor
+            className="editor"
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
