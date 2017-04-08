f4817b32b69f4eb6f41104007150aec7a784859c
Daniel Stein <stein-daniel@codingcircus.com>
2017-04-08 19:01:21 +0200

    [TASK] Styling and separate Editor Component

--- src/App.js
+++ src/App.js
@@ -1,81 +1,9 @@
 import React, { Component } from 'react';
-import Editor from 'draft-js-plugins-editor';
-import { EditorState, RichUtils } from 'draft-js';
-import createEmojiPlugin from 'draft-js-emoji-plugin';
-import createInlineToolbarPlugin, { Separator } from 'draft-js-inline-toolbar-plugin';
-import 'draft-js-emoji-plugin/lib/plugin.css';
-import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
-import {
-  ItalicButton,
-  BoldButton,
-  CodeButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-  CodeBlockButton,
-} from 'draft-js-buttons';
 import logo from './logo.svg';
-import './App.css';
-
-const emojiPlugin = createEmojiPlugin();
-const { EmojiSuggestions } = emojiPlugin;
-const inlineToolbarPlugin = createInlineToolbarPlugin({
-  structure: [
-    HeadlineOneButton,
-    HeadlineTwoButton,
-    HeadlineThreeButton,
-    Separator,
-    BoldButton,
-    ItalicButton,
-    UnorderedListButton,
-    OrderedListButton,
-    CodeButton,
-    Separator,
-    BlockquoteButton,
-    CodeBlockButton,
-  ]
-});
-const { InlineToolbar } = inlineToolbarPlugin;
-
-const plugins = [
-  emojiPlugin,
-  inlineToolbarPlugin,
-];
+import './css/App.css';
+import Editor from './Components/Editor';
 
 class App extends Component {
-
-  state = {
-    editorState: EditorState.createEmpty(),
-  };
-
-  onChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  focus = () => {
-    this.editor.focus();
-  }
-
-  componentDidMount() {
-    const { editor } = this;
-    if (editor) {
-      setTimeout(editor.focus.bind(editor), 1000);
-    }
-  }
-
-  handleKeyCommand = (command) => {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  }
 
   render() {
     return (
@@ -84,17 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Editor Test</h2>
         </div>
-        <div className="App-content" onClick={this.focus}>
-          <Editor
-            editorState={this.state.editorState}
-            handleKeyCommand={this.handleKeyCommand}
-            onChange={this.onChange}
-            plugins={plugins}
-            ref={(element) => { this.editor = element; }}
-            />
-          <EmojiSuggestions />
-          <InlineToolbar />
-        </div>
+        <Editor />
       </div>
     );
   }
